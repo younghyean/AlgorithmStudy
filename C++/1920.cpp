c7ad64421769215122cbd8d33a0f8899e716441e
yonghyen <ankim2son@gmail.com>
2021-05-23 19:27:17 +0900

    no message

---
+++ C++/1920.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int N, M;
+	set<int> a;
+	vector<int> b;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		a.insert(temp);
+	}
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		int temp;
+		cin >> temp;
+		b.push_back(temp);
+	}
+	for (auto x : b) {
+		if (a.count(x)) cout << "1\n";
+		else cout << "0\n";
+	}
+}
